c76858b15190c06ac1ecbb38471f091e4201cc63
AniruddhaHaval <havalaniruddha@gmail.com>
2024-03-24 19:16:29 +0530

    primitive data types understood

--- primitive_data_types.py
+++ primitive_data_types.py
@@ -1,11 +1,38 @@
+print(len("Hello"))
+# print(len(1234))
+# TypeError: object of type 'int' has no len()
+
+
+
+
 # String
 
+print("Hello"[1])
+
 print("Hello"[0])
+
+print("Hello"[4])
 
 print("123" + "456")
 
 # Integer
 
+print(5+2)
+
 print(123 + 7)
 
 print(123_000 + 6)
+
+print(10_000 + 100)
+
+# Float
+pi = 3.14
+print(type(pi))
+print(3.14159)
+
+
+# Boolean
+
+condition = True
+print(type(condition))
+False
